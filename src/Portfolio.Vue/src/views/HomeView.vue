316206b49ee6a15bbcf4cad307df9480a86fcf8f
Callum <Caza390plays@gmail.com>
2024-01-09 19:41:32 +0000

    Updated the main page. Caz is much more centred and better padded. Contact buttons are more thicker and easier to click. Hyper-links are more easy to select. text is further from the wall. Image and text box is more closer, this also solves the issue that might happen when using different monitors.

--- src/Portfolio.Vue/src/views/HomeView.vue
+++ src/Portfolio.Vue/src/views/HomeView.vue
@@ -9,23 +9,23 @@ const openPdf = () => {
 <template>
   <Body class="min-h-screen bg-woodsmoke-950">
     <header class="flex justify-between h-max bg-black">
-      <RouterLink to="/" class="flex justify-start items-center text-8xl text-pf-red-900 font-verdana font-bold border-r-4 border-pf-red-950 my-1 ml-7 pr-3"
+      <RouterLink to="/" class="flex justify-start items-center text-8xl text-pf-red-900 font-verdana font-bold border-r-4 border-pf-red-950 my-2 ml-7 pb-2 px-3"
       >Caz</RouterLink>
 
       <!--  Tabs  -->
       <div class="flex justify-between items-center">
-        <RouterLink to="/projects" class="text-2xl text-white font-verdana font-bold border-r border-pf-red-950 py-1 px-4 hover:border-b-2"
+        <RouterLink to="/projects" class="text-2xl text-white font-verdana font-bold border-r border-pf-red-950 py-2 px-4 hover:border-b-2"
         >Projects</RouterLink>
-        <RouterLink to="/game_dev" class="text-2xl text-white font-verdana font-bold border-x border-pf-red-950 py-1 px-4 hover:border-b-2"
+        <RouterLink to="/game_dev" class="text-2xl text-white font-verdana font-bold border-x border-pf-red-950 py-2 px-4 hover:border-b-2"
         >Game Dev</RouterLink>
-        <RouterLink to="/3d_modelling" class="text-2xl text-white font-verdana border-x border-pf-red-950 font-bold py-1 px-4 hover:border-b-2"
+        <RouterLink to="/3d_modelling" class="text-2xl text-white font-verdana border-x border-pf-red-950 font-bold py-2 px-4 hover:border-b-2"
         >Models</RouterLink>
-        <button @click="openPdf" class="text-2xl text-white font-verdana border-l border-pf-red-950 font-bold py-1 px-4 hover:border-b-2"
+        <button @click="openPdf" class="text-2xl text-white font-verdana border-l border-pf-red-950 font-bold py-2 px-4 hover:border-b-2"
         >CV</button>
       </div>
 
       <div class="flex justify-end items-center my-1 mr-7">
-        <RouterLink to="/contact_me" class="text-2xl text-pf-red-300 font-verdana font-bold bg-pf-red-950 rounded-xl py-1 px-2 hover:bg-pf-red-900"
+        <RouterLink to="/contact_me" class="text-2xl text-pf-red-300 font-verdana font-bold bg-pf-red-950 rounded-xl py-3 px-4 hover:bg-pf-red-900"
         >Contact Me</RouterLink>
       </div>
     </header>
