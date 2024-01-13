8deb662063cc9b8c8d5f3f88cf3ddc663eb683c2
Callum <Caza390plays@gmail.com>
2024-01-13 13:16:53 +0000

    Made the temporary stuff more understandable for the page

--- src/Portfolio.Vue/src/components/Software/3dModelling/3dModelling.vue
+++ src/Portfolio.Vue/src/components/Software/3dModelling/3dModelling.vue
@@ -117,8 +117,9 @@ onMounted(() => {
 
   <!-- Phone View -->
   <body v-else>
-    <div>
-      <p class="text-white text-2xl font-verdana font-bold m-10">Please access site on desktop to see this page</p>
+    <div class="m-10">
+      <h2 class="text-white text-4xl font-verdana font-bold mb-3">3D Modelling:</h2>
+      <p class="text-white text-2xl font-verdana font-bold">Please access site on desktop to see this page</p>
     </div>
   </body>
 </template>
