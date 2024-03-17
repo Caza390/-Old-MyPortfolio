7866c3a4af51a2abf5fccd339a73ca581f77aa00
Callum <Caza390plays@gmail.com>
2024-03-17 21:34:27 +0000

    Grammar

--- src/Portfolio.Vue/src/components/About.vue
+++ src/Portfolio.Vue/src/components/About.vue
@@ -20,7 +20,7 @@ watch(screenSize, () => {
           <h2 class="text-white text-6xl font-verdana font-bold">Hello, my name is Callum Carter-Begbie. This is my portfolio!</h2>
           <br>
           <p class="text-pf-red-400 text-3xl font-helvetica">
-          I am someone who is dedicated to achieving thair best at all times. There are many projects I like to get my hands involved in ranging from music, computer projects, cars or even real world projects like 3D printing and room lighting.
+          I am someone who is dedicated to achieving their best at all times. There are many projects I like to get my hands involved in ranging from music, computer projects, cars or even real world projects like 3D printing and room lighting.
           <br><br>
           When working in the automotive industy (paint in particular). I found myself getting involved in something technological with software or any other computing related stuff - hence pushing for a software career
           </p>
@@ -56,7 +56,7 @@ watch(screenSize, () => {
         <h2 class="text-white text-2xl font-verdana font-bold">Hello, my name is Callum Carter-Begbie. This is my portfolio!</h2>
           <br>
           <p class="text-pf-red-400 text-lg font-helvetica">
-          I am someone who is dedicated to achieving thair best at all times. There are many projects I like to get my hands involved in ranging from music, computer projects, cars or even real world projects like 3D printing and room lighting.
+          I am someone who is dedicated to achieving their best at all times. There are many projects I like to get my hands involved in ranging from music, computer projects, cars or even real world projects like 3D printing and room lighting.
           <br><br>
           When working in the automotive industy (paint in particular). I found myself getting involved in something technological with software or any other computing related stuff - hence pushing for a software career
           </p>
